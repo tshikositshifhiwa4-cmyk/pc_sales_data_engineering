3f7d27d9ed30b5458dc2a6a1ebfa4750277ce2b8
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-14 13:22:45 +0200

    adding 4.dim_store.sql into dimension tables folder

---
+++ creating_tables/dimension_tables/4.dim_store.sql
@@ -0,0 +1,13 @@
+--5.dim_store
+drop table [pc_staging].[dbo].[dim_store]
+create table [pc_staging].[dbo].[dim_store](
+	[store_id] int identity(1,1) primary key,
+	[shop_name] [nvarchar](50) NOT NULL,
+	[shop_age] [nvarchar](50) NOT NULL,
+)
+insert into [pc_staging].[dbo].[dim_store] (shop_name, shop_age)
+select distinct shop_name, shop_age
+from [pc_staging].[dbo].[pc_data];
+
+	--validating the insert
+		select * from [pc_staging].[dbo].[dim_store];
