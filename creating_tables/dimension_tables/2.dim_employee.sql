6c91ad0b6c203770e141b02f06398478abef4d86
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-14 13:21:08 +0200

    adding 2.dim_employye.sql into dimension tables folder

---
+++ creating_tables/dimension_tables/2.dim_employee.sql
@@ -0,0 +1,13 @@
+--2.dim_employee
+drop table [pc_staging].[dbo].[dim_employee]
+create table [pc_staging].[dbo].[dim_employee](
+	[employee_id] int identity(1,1) primary key,
+	[employee_name] [nvarchar](50) NOT NULL,
+	[department] [nvarchar](50) NOT NULL
+)
+insert into [pc_staging].[dbo].[dim_employee] (employee_name, department)
+select distinct Sales_Person_Name, Sales_Person_Department 
+from [pc_staging].[dbo].[pc_data];
+
+	--validating the insert
+		select * from [pc_staging].[dbo].[dim_employee];
