39971a133338105342a735d79dc4cda993347a52
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-14 13:26:08 +0200

    adding 8.dim_priority.sql into dimension tables folder

---
+++ creating_tables/dimension_tables/8.dim_priority.sql
@@ -0,0 +1,12 @@
+--8.dim_priority
+drop table [pc_staging].[dbo].[dim_priority]
+create table [pc_staging].[dbo].[dim_priority](
+	[priority_id] int identity(1,1) primary key,
+	[Priority] [nvarchar](50) NOT NULL,
+)
+insert into [pc_staging].[dbo].[dim_priority] (priority)
+select distinct priority
+from [pc_staging].[dbo].[pc_data];
+
+	--validating the insert
+		select * from [pc_staging].[dbo].[dim_priority];
