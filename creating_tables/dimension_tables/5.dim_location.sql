28bbf1cc63025d5218ea2d5c803ab9e4d200cad7
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-14 13:23:38 +0200

    adding 5.dim_location.sql into dimension tables folder

---
+++ creating_tables/dimension_tables/5.dim_location.sql
@@ -0,0 +1,14 @@
+--4.dim_location
+drop table [pc_staging].[dbo].[dim_location]
+create table [pc_staging].[dbo].[dim_location](
+	[location_id] int identity(1,1) primary key,
+	[continent] [nvarchar](50) NOT NULL,
+	[country_or_state] [nvarchar](50) NOT NULL,
+	[province_or_city] [nvarchar](100) NOT NULL
+)
+insert into [pc_staging].[dbo].[dim_location] (continent, country_or_state, province_or_city)
+select distinct continent, country_or_state, province_or_city
+from [pc_staging].[dbo].[pc_data];
+
+	--validating the insert
+		select * from [pc_staging].[dbo].[dim_location];
