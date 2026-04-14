456dc92ed25b05888bc06fd3d9ca77c1652ce74a
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-14 13:21:58 +0200

    adding 3.dim_product.sql into dimension tables folder

---
+++ creating_tables/dimension_tables/3.dim_product.sql
@@ -0,0 +1,16 @@
+--3.dim_product
+drop table [pc_staging].[dbo].[dim_product]
+create table [pc_staging].[dbo].[dim_product](
+	[product_id] int identity(1,1) primary key,
+	[pc_make] [nvarchar](50) NOT NULL,
+	[pc_model] [nvarchar](50) NOT NULL,
+	[storage_type] [nvarchar](50) NOT NULL,
+	[storage_capacity] [nvarchar](50) NOT NULL,
+	[ram] [nvarchar](50) NOT NULL
+)
+insert into [pc_staging].[dbo].[dim_product] (pc_make, pc_model, storage_type, storage_capacity, ram)
+select distinct pc_make, pc_model, storage_type, storage_capacity, ram 
+from [pc_staging].[dbo].[pc_data];
+
+	--validating the insert
+		select * from [pc_staging].[dbo].[dim_product];
