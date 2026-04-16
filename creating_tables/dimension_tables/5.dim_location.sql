6ddd77bebe9a3547c4f7e19525ccf1d4009c1bb7
Princess <tshikositshifhiwa4@gmail.com>
2026-04-17 00:07:42 +0200

    updated 5.dim_location table to include the load_date datetime

--- creating_tables/dimension_tables/5.dim_location.sql
+++ creating_tables/dimension_tables/5.dim_location.sql
@@ -4,7 +4,8 @@ create table [pc_staging].[dbo].[dim_location](
 	[location_id] int identity(1,1) primary key,
 	[continent] [nvarchar](50) NOT NULL,
 	[country_or_state] [nvarchar](50) NOT NULL,
-	[province_or_city] [nvarchar](100) NOT NULL
+	[province_or_city] [nvarchar](100) NOT NULL,
+	[load_date] datetime default getdate()
 )
 insert into [pc_staging].[dbo].[dim_location] (continent, country_or_state, province_or_city)
 select distinct continent, country_or_state, province_or_city
