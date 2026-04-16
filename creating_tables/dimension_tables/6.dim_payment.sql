e0821e3d51e2b563e22b8893ae7dc6de0b4d4d27
Princess <tshikositshifhiwa4@gmail.com>
2026-04-17 00:10:02 +0200

    updated 5.dim_payment table to include the load_date datetime

--- creating_tables/dimension_tables/6.dim_payment.sql
+++ creating_tables/dimension_tables/6.dim_payment.sql
@@ -3,6 +3,7 @@ drop table [pc_staging].[dbo].[dim_payment]
 create table [pc_staging].[dbo].[dim_payment](
 	[payment_id] int identity(1,1) primary key,
 	[Payment_Method] [nvarchar](50) NOT NULL,
+	[load_date] datetime default getdate()
 )
 insert into [pc_staging].[dbo].[dim_payment] (payment_method)
 select distinct payment_method
