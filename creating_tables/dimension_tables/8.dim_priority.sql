5e3512e9f2587634eb8c915698f7d33da9f93522
Princess <tshikositshifhiwa4@gmail.com>
2026-04-17 00:15:04 +0200

    updated 8.dim_priority table to include the load_date datetime

--- creating_tables/dimension_tables/8.dim_priority.sql
+++ creating_tables/dimension_tables/8.dim_priority.sql
@@ -3,6 +3,7 @@ drop table [pc_staging].[dbo].[dim_priority]
 create table [pc_staging].[dbo].[dim_priority](
 	[priority_id] int identity(1,1) primary key,
 	[Priority] [nvarchar](50) NOT NULL,
+	[load_date] datetime default getdate()
 )
 insert into [pc_staging].[dbo].[dim_priority] (priority)
 select distinct priority
