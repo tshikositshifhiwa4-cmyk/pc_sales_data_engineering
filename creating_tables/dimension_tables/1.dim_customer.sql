987074767958e129ce58a688fee1614a414daec6
Princess <tshikositshifhiwa4@gmail.com>
2026-04-16 23:36:19 +0200

    updated dim_customer table to include the load_date datetime

--- creating_tables/dimension_tables/1.dim_customer.sql
+++ creating_tables/dimension_tables/1.dim_customer.sql
@@ -5,7 +5,8 @@ create table [pc_staging].[dbo].[dim_customer](
 	[first_name] [nvarchar](50) NOT NULL,
 	[last_name] [nvarchar](50) NOT NULL,
 	[contact_number] [nvarchar](50) NOT NULL,
-	[email_address] [nvarchar](50) NOT NULL
+	[email_address] [nvarchar](50) NOT NULL,
+	[load_date] datetime default getdate()
 )
 insert into [pc_staging].[dbo].[dim_customer] (first_name, last_name, contact_number, email_address )
 select distinct Customer_Name, Customer_Surname, Customer_Contact_Number, Customer_Email_Address
