a24a86f1093bed0fa1b428f280b8ab4f39cc2ca5
Princess <tshikositshifhiwa4@gmail.com>
2026-04-16 23:40:28 +0200

    updated dim_employee table to include the load_date datetime

--- creating_tables/dimension_tables/2.dim_employee.sql
+++ creating_tables/dimension_tables/2.dim_employee.sql
@@ -3,7 +3,8 @@ drop table [pc_staging].[dbo].[dim_employee]
 create table [pc_staging].[dbo].[dim_employee](
 	[employee_id] int identity(1,1) primary key,
 	[employee_name] [nvarchar](50) NOT NULL,
-	[department] [nvarchar](50) NOT NULL
+	[department] [nvarchar](50) NOT NULL,
+	[load_date] datetime default getdate()
 )
 insert into [pc_staging].[dbo].[dim_employee] (employee_name, department)
 select distinct Sales_Person_Name, Sales_Person_Department 
