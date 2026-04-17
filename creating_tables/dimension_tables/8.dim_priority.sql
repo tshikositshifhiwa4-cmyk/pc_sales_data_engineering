71b05f83fdd685d5ff9f02ec48611f36ee9278fa
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 16:33:07 +0200

    formatting code for 8.dim_priority table

--- creating_tables/dimension_tables/8.dim_priority.sql
+++ creating_tables/dimension_tables/8.dim_priority.sql
@@ -1,13 +1,18 @@
 --8.dim_priority
-drop table [pc_staging].[dbo].[dim_priority]
-create table [pc_staging].[dbo].[dim_priority](
-	[priority_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_priority] create table [pc_staging].[dbo].[dim_priority](
+	[priority_id] int identity(1, 1) primary key,
 	[Priority] [nvarchar](50) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_priority] (priority)
-select distinct priority
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_priority] (priority)
+select
+	distinct priority
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-		select * from [pc_staging].[dbo].[dim_priority];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_priority];
