45e6c9d5023cc50cf57c713a352ff19b471a0aa0
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 16:30:03 +0200

    formatting code for 5.dim_location

--- creating_tables/dimension_tables/5.dim_location.sql
+++ creating_tables/dimension_tables/5.dim_location.sql
@@ -1,15 +1,23 @@
---4.dim_location
+--4.dim_location drop TABLE [pc_staging].[dbo].[dim_location]
 drop table [pc_staging].[dbo].[dim_location]
 create table [pc_staging].[dbo].[dim_location](
-	[location_id] int identity(1,1) primary key,
+	[location_id] int identity(1, 1) primary key,
 	[continent] [nvarchar](50) NOT NULL,
 	[country_or_state] [nvarchar](50) NOT NULL,
 	[province_or_city] [nvarchar](100) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_location] (continent, country_or_state, province_or_city)
-select distinct continent, country_or_state, province_or_city
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_location] (continent, country_or_state, province_or_city)
+select
+	distinct continent,
+	country_or_state,
+	province_or_city
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-		select * from [pc_staging].[dbo].[dim_location];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_location];
