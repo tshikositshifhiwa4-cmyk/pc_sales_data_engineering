488b0d1ebfd18577d63966541f9a2a66b01ce4bd
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 16:33:37 +0200

    formatting code for 9.dim_date table

--- creating_tables/dimension_tables/9.dim_date.sql
+++ creating_tables/dimension_tables/9.dim_date.sql
@@ -1,17 +1,24 @@
 --9.dim_date
-drop table [pc_staging].[dbo].[dim_date]
-create table [pc_staging].[dbo].[dim_date](
-	[date_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_date] create table [pc_staging].[dbo].[dim_date](
+	[date_id] int identity(1, 1) primary key,
 	[purchase_date] [date] NOT NULL,
 	[ship_date] [date] NULL,
 	[year] int NOT NULL,
 	[month] int NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_date] (purchase_date, ship_date, year, month)
-select distinct purchase_date, coalesce(try_cast(ship_date as date), '9999-12-31'), 
-				year(purchase_date), month(purchase_date)
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_date] (purchase_date, ship_date, year, month)
+select
+	distinct purchase_date,
+	coalesce(try_cast(ship_date as date), '9999-12-31'),
+	year(purchase_date),
+	month(purchase_date)
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-			select * from  [pc_staging].[dbo].[dim_date];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_date];
