8368fe31ae01915e098749c9ef4d8ccb66a68a4b
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 14:52:05 +0200

    formattinf code for 2.dim_employee table

--- creating_tables/dimension_tables/2.dim_employee.sql
+++ creating_tables/dimension_tables/2.dim_employee.sql
@@ -1,14 +1,20 @@
 --2.dim_employee
-drop table [pc_staging].[dbo].[dim_employee]
-create table [pc_staging].[dbo].[dim_employee](
-	[employee_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_employee] create table [pc_staging].[dbo].[dim_employee](
+	[employee_id] int identity(1, 1) primary key,
 	[employee_name] [nvarchar](50) NOT NULL,
 	[department] [nvarchar](50) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_employee] (employee_name, department)
-select distinct Sales_Person_Name, Sales_Person_Department 
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_employee] (employee_name, department)
+select
+	distinct Sales_Person_Name,
+	Sales_Person_Department
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert into table
-		select * from [pc_staging].[dbo].[dim_employee];
+--validating the insert into table
+select
+	*
+from
+	[pc_staging].[dbo].[dim_employee];
