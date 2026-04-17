e722ed1fa700850234548a24ceec3244545a8947
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 16:30:50 +0200

    formatting code for 6.dim_payment table

--- creating_tables/dimension_tables/6.dim_payment.sql
+++ creating_tables/dimension_tables/6.dim_payment.sql
@@ -1,13 +1,18 @@
 --6.dim_payment
-drop table [pc_staging].[dbo].[dim_payment]
-create table [pc_staging].[dbo].[dim_payment](
-	[payment_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_payment] create table [pc_staging].[dbo].[dim_payment](
+	[payment_id] int identity(1, 1) primary key,
 	[Payment_Method] [nvarchar](50) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_payment] (payment_method)
-select distinct payment_method
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_payment] (payment_method)
+select
+	distinct payment_method
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-		select * from [pc_staging].[dbo].[dim_payment];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_payment];
