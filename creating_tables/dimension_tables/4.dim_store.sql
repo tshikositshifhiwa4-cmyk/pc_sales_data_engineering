3e53eccb2ecc8108076e7680ff05cc271cd240af
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 15:12:56 +0200

    formatting code for 4.dim_store table

--- creating_tables/dimension_tables/4.dim_store.sql
+++ creating_tables/dimension_tables/4.dim_store.sql
@@ -1,5 +1,6 @@
 --5.dim_store
-drop table [pc_staging].[dbo].[dim_store] create table [pc_staging].[dbo].[dim_store](
+drop table [pc_staging].[dbo].[dim_store] 
+create table [pc_staging].[dbo].[dim_store](
 	[store_id] int identity(1, 1) primary key,
 	[shop_name] [nvarchar](50) NOT NULL,
 	[shop_age] [nvarchar](50) NOT NULL,
