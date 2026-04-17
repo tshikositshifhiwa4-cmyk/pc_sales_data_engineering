869784a57ad7e648c23817d9e29edddbe78fdbb2
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 14:50:47 +0200

    correcting code formatting for 1.dim_customer table

--- creating_tables/dimension_tables/1.dim_customer.sql
+++ creating_tables/dimension_tables/1.dim_customer.sql
@@ -1,16 +1,29 @@
 --1. dim_customers
-drop table [pc_staging].[dbo].[dim_customer]
-create table [pc_staging].[dbo].[dim_customer](
-	[customer_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_customer] create table [pc_staging].[dbo].[dim_customer](
+	[customer_id] int identity(1, 1) primary key,
 	[first_name] [nvarchar](50) NOT NULL,
 	[last_name] [nvarchar](50) NOT NULL,
 	[contact_number] [nvarchar](50) NOT NULL,
 	[email_address] [nvarchar](50) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_customer] (first_name, last_name, contact_number, email_address )
-select distinct Customer_Name, Customer_Surname, Customer_Contact_Number, Customer_Email_Address
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_customer] (
+		first_name,
+		last_name,
+		contact_number,
+		email_address
+	)
+select
+	distinct Customer_Name,
+	Customer_Surname,
+	Customer_Contact_Number,
+	Customer_Email_Address
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-		select * from [pc_staging].[dbo].[dim_customer];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_customer];
