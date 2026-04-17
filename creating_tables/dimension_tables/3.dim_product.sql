354d62ea4d5492d0f7d935022fde8774445428dd
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 15:03:30 +0200

    formatted the code for 3.dim_product table

--- creating_tables/dimension_tables/3.dim_product.sql
+++ creating_tables/dimension_tables/3.dim_product.sql
@@ -1,7 +1,6 @@
 --3.dim_product
-drop table [pc_staging].[dbo].[dim_product]
-create table [pc_staging].[dbo].[dim_product](
-	[product_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_product] create table [pc_staging].[dbo].[dim_product](
+	[product_id] int identity(1, 1) primary key,
 	[pc_make] [nvarchar](50) NOT NULL,
 	[pc_model] [nvarchar](50) NOT NULL,
 	[storage_type] [nvarchar](50) NOT NULL,
@@ -9,9 +8,25 @@ create table [pc_staging].[dbo].[dim_product](
 	[ram] [nvarchar](50) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_product] (pc_make, pc_model, storage_type, storage_capacity, ram)
-select distinct pc_make, pc_model, storage_type, storage_capacity, ram 
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_product] (
+		pc_make,
+		pc_model,
+		storage_type,
+		storage_capacity,
+		ram
+	)
+select
+	distinct pc_make,
+	pc_model,
+	storage_type,
+	storage_capacity,
+	ram
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-		select * from [pc_staging].[dbo].[dim_product];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_product];
