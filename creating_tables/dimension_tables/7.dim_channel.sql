07941e1e15d2add9fef306dc9a3f25727aac35fd
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 16:31:38 +0200

    formatting code for 7.dim_channel table

--- creating_tables/dimension_tables/7.dim_channel.sql
+++ creating_tables/dimension_tables/7.dim_channel.sql
@@ -1,13 +1,18 @@
 --7.dim_channel
-drop table [pc_staging].[dbo].[dim_channel]
-create table [pc_staging].[dbo].[dim_channel](
-	[channel_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_channel] create table [pc_staging].[dbo].[dim_channel](
+	[channel_id] int identity(1, 1) primary key,
 	[Channel] [nvarchar](50) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_channel] (channel)
-select distinct channel
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_channel] (channel)
+select
+	distinct channel
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-		select * from [pc_staging].[dbo].[dim_channel];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_channel];
