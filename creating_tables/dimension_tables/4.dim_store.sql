abcd092dbbabf9112c7052c70752f11f86a0acc2
Tshifhiwa Tshikosi <tshikositshifhiwa4@gmail.com>
2026-04-17 15:06:32 +0200

    formatted 4.dim_store table code

--- creating_tables/dimension_tables/4.dim_store.sql
+++ creating_tables/dimension_tables/4.dim_store.sql
@@ -1,14 +1,20 @@
 --5.dim_store
-drop table [pc_staging].[dbo].[dim_store]
-create table [pc_staging].[dbo].[dim_store](
-	[store_id] int identity(1,1) primary key,
+drop table [pc_staging].[dbo].[dim_store] create table [pc_staging].[dbo].[dim_store](
+	[store_id] int identity(1, 1) primary key,
 	[shop_name] [nvarchar](50) NOT NULL,
 	[shop_age] [nvarchar](50) NOT NULL,
 	[load_date] datetime default getdate()
 )
-insert into [pc_staging].[dbo].[dim_store] (shop_name, shop_age)
-select distinct shop_name, shop_age
-from [pc_staging].[dbo].[pc_data];
+insert into
+	[pc_staging].[dbo].[dim_store] (shop_name, shop_age)
+select
+	distinct shop_name,
+	shop_age
+from
+	[pc_staging].[dbo].[pc_data];
 
-	--validating the insert
-		select * from [pc_staging].[dbo].[dim_store];
+--validating the insert
+select
+	*
+from
+	[pc_staging].[dbo].[dim_store];
